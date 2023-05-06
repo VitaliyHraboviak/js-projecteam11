16314f27e31c9634402abaefee457e0a6830a780
VitaliyHraboviak <othet1976@gmail.com>
2023-05-06 14:58:24 +0300

    update: src/images/icon , supportUkraine. html

---
+++ src/js/gallery-supportukrain.js
@@ -0,0 +1,50 @@
+ import IMAGES from '../images/icon/index.js';
+
+
+export const gallerysupport = [
+{
+title: 'Save the Children',
+url:'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+img: `${IMAGES.SavetheChildren}`,
+},
+{
+title: 'Project HOPE',
+url: 'https://www.projecthope.org/country/ukraine/',
+img: `${IMAGES.HOPE}`,
+},
+{
+title: 'UNITED24',
+url: 'https://u24.gov.ua/uk',
+img: `${IMAGES.UNiTED}`,
+},
+{
+title: 'International Medical Corps',
+url: 'https://internationalmedicalcorps.org/country/ukraine/',
+img: `${IMAGES.InternationalMedicalCorps}`,
+},
+{
+title: 'Medicins Sans Frontieres',
+url: 'https://www.msf.org/ukraine',
+img: `${IMAGES.MedicalSansFrontieres}`,
+},
+{
+title: 'RAZOM',
+url: 'https://www.razomforukraine.org/',
+img: `${IMAGES.RAZOM}`,
+},
+{
+title: 'Action against hunger',
+url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+img: `${IMAGES.ACTIONAGAINSTHUNGER}`,
+},
+{
+title: 'World vision',
+url: 'https://www.wvi.org/emergencies/ukraine',
+img: `${IMAGES.WorldVision}`,
+},
+{
+title: 'Serhiy Prytula Charity Foundation',
+url: 'https://prytulafoundation.org/en',
+img: `${IMAGES.SERGIYPRYTULA}`,
+},
+]
