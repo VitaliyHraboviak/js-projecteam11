44869f45edcba24c2e51129584d7ae116ff79a6c
VitaliyHraboviak <othet1976@gmail.com>
2023-05-07 17:19:25 +0300

    Update support

--- src/js/01-gallerysupportukrain.js
+++ src/js/01-gallerysupportukrain.js
@@ -1,14 +1,15 @@
 import { gallerysupport } from './gallery-supportukrain';
+import Slick from "slick-carousel";
 
 const galleryEl = document.querySelector('.gallery-support');
 const itemsGalleryEl = createItemsGallery(gallerysupport);
 galleryEl.insertAdjacentHTML('beforeend', itemsGalleryEl);
-galleryEl.addEventListener('click', onImgClick);
+// galleryEl.addEventListener('click', onImgClick);
 
 function createItemsGallery(items) {
     return items
     .map(({ title, url, img }) => {
-      return `<li class="gallery__item_support">
+      return `<li class="gallery__item_support fade">
   <a class="gallery__link" href="${url}">
     <img
       class="gallery__image"
@@ -19,40 +20,6 @@ function createItemsGallery(items) {
     })
     .join('');
 };    
+ 
 
-var slider = document.querySelector('.gallery-support');
-console.log(slider);
-var slides = slider.querySelectorAll('.gallery__item_support');
-console.log(slides);
-var slideCount = slides.length;
-console.log(slideCount);
-var visibleSlides = 6;
-var currentSlide = 0;
-
-function showSlides() {
-    for (var i = 0; i < slideCount; i++) {
-        slides[i].style.display = 'none';
-    }
-    for (var i = currentSlide; i < currentSlide + visibleSlides; i++) {
-     
-        if (slides[i]) {
-            slides[i].style.display = 'flex';
-            
-        }
-     
-    }
-}
-
-showSlides();
-
-
-const carusel = document.querySelector('.carousel-button-next');
-
-    carusel.addEventListener('click', function () {
-    if (currentSlide > 0) {
-        currentSlide++;
-        showSlides();
-        } 
-});
-
-
+		
